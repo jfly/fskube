bdff420cb7d01bc3ad57066ef68ed405960f2582
Jeremy Fleischman <jeremyfleischman@gmail.com>
2014-05-05 02:23:39 -0700

    ModemRoundtripTest.py now fails =(. I'm pretty sure we have this same bug in the original javascript code, so nothing to worry about =).

--- test/ModemRoundtripTest.py
+++ test/ModemRoundtripTest.py
@@ -13,12 +13,14 @@ class Capturer(fskube.boolReceiver):
         self.bits = []
 
     def receive(self, bit):
-        self.bits.append(bit)
+        self.bits.append(bit + 0)
 
 class RoundtripTest(unittest.TestCase):
-    def test(self):
+    
+    def doRoundtrip(self, samplesPerSecond, bits):
+        print("Roundtrip test at %shZ" % samplesPerSecond)
         fskParams = fskube.FskParams()
-        fskParams.samplesPerSecond = 41000
+        fskParams.samplesPerSecond = samplesPerSecond
         fskParams.bitsPerSecond = 1200
         fskParams.markFrequency = 2200
         fskParams.spaceFrequency = 1200
@@ -30,13 +32,17 @@ class RoundtripTest(unittest.TestCase):
         modulator.connect(demodulator)
         demodulator.connect(c)
 
-        bits = [1, 0, 1]
         for bit in bits:
             modulator.receive(bit)
 
         demodulator.flush()
 
         self.assertEqual(c.bits, bits)
+
+    def test(self):
+        bits = [0,1,0,0,0,0,0,1,0,1,0,0,0,0,1]
+        for samplesPerSecond in [ 48000, 44100, 16000 ]:
+            self.doRoundtrip(samplesPerSecond, bits)
 
 if __name__ == "__main__":
     unittest.main()
